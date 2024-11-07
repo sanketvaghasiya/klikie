1f17cf8e573f4519b2df417ec254e601825a44bc
Sanket Vaghasiya <sanketvaghasiya08@gmail.com>
2024-11-08 04:51:54 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+  "use strict";
+
+  //===== Prealoder
+
+  $(window).on("load", function (event) {
+    $(".preloader").delay(500).fadeOut(500);
+  });
+
+  //=====  WOW active
+
+  //   new WOW().init();
+
+  $(".testimonial_slider").slick({
+    prevArrow: $(".slick-prev"),
+    nextArrow: $(".slick-next"),
+    dots: false,
+    infinite: false,
+    speed: 500,
+    fade: false,
+    cssEase: "linear",
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+    ],
+  });
+});
